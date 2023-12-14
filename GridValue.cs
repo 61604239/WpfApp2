958b705a1a114f03b7eafc380000fc7ba971d6d3
61604239CTC <61604239CTC@MA-CTC-SDD-S11>
2023-12-14 09:27:19 -0500

    Add project files.

---
+++ GridValue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public enum GridValue
+    {
+        Empty,
+        Snake,
+        Food,
+        Outside,
+    }
+}
